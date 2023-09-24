50517597723d7566517cec6aa09d39c3b6532d68
kemishigp <kemish.gip@gmail.com>
2023-09-23 19:06:41 -0600

    push

---
+++ w02-task/scripts/w02-task.js
@@ -0,0 +1,39 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = 'Kemish Gomez'
+// Get the current date
+const currentDate = new Date();
+
+// Extract the current year from the date object
+const currentYear = currentDate.getFullYear();
+
+// Print the current year to the console
+console.log("Current Year:", currentYear);
+const profilePicture = "images/Nevado.jpg";
+
+
+
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById("food");
+const yearElement = document.querySelector("#year");
+
+
+/* Step 4 - Adding Content */
+
+
+
+
+
+
+/* Step 5 - Array */
+
+
+
+
+
+
