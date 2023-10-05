7f443d6f2fa12fe4d9b7ea55df9f7a9e7f7985c7
kemishigp <kemish.gip@gmail.com>
2023-10-04 19:25:55 -0600

    places

---
+++ scripts/w04-task.js
@@ -0,0 +1,40 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+
+let myProfile = {name:"Kemish",
+photo:"/Users/kemish/Documents/CODING_HW/cse121b/images/Nevado.jpg",
+favoriteFoods:["Pizza", "Sushi", "Tacos"],
+hobbies:["Reading", "Running", "Piano"],
+placesLived:[]
+};
+g
+// This occurs outside of the object definition.
+myProfile.placesLived.push(
+  {
+    place: 'Mexico City',
+    length: '3 years'
+  }
+);
+
+/* Populate Profile Object with placesLive objects */
+
+
+
+
+/* DOM Manipulation - Output */
+
+/* Name */
+
+/* Photo with attributes */
+
+
+/* Favorite Foods List*/
+
+
+/* Hobbies List */
+
+
+/* Places Lived DataList */
+
+
