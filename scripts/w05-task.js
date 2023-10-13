1147db761e4ab0ca727222ce2e52acba90d304c2
kemishigp <kemish.gip@gmail.com>
2023-10-12 21:29:14 -0600

    foreach

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -7,33 +7,26 @@ let templeList = [];
 
 /* async displayTemples Function */
 const displayTemples = (temples) => {
-    // Your function code here
-    // You can access the list of temples using the templeList parameter
+    temples.forEach((temple) => {
+      const articleElement = document.createElement('article');
+      
+      const h3Element = document.createElement('h3');
+      h3Element.textContent = temple.templeName;
+      
+      const imgElement = document.createElement('img');
+      imgElement.src = temple.imageUrl;
+      imgElement.alt = temple.location;
+      
+      articleElement.appendChild(h3Element);
+      articleElement.appendChild(imgElement);
+      
+      templesElement.appendChild(articleElement);
+    });
   };
-
-// Iterate through the array of temples using forEach
-temples.forEach((temple) => {
-    // Create an HTML <article> element
-    templesElement.appendChild(article);
-    
-    const articleElement = document.createElement('article');
   
-    // Create an HTML <h3> element and add the temple's templeName property to it
-    const h3Element = document.createElement('h3');
-    h3Element.textContent = temple.templeName;
+  // Call the function with the array of temples
+  displayTemples(temples);
   
-    // Create an HTML <img> element
-    const imgElement = document.createElement('img');
-    imgElement.src = temple.imageUrl; // Add the imageUrl property to the src attribute
-    imgElement.alt = temple.location;  // Add the location property to the alt attribute
-  
-    // Append the <h3> element and the <img> element to the <article> element
-    articleElement.appendChild(h3Element);
-    articleElement.appendChild(imgElement);
-  
-    // Append the <article> element to the global templesElement variable
-    templesElement.appendChild(articleElement);
-  });
   
 
 /* async getTemples Function using fetch()*/
