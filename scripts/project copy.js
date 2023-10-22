f8b270d7233fe3ffef22b6be00578e0ac2db636d
kemishigp <kemish.gip@gmail.com>
2023-10-22 17:03:33 -0600

    fetch

---
+++ scripts/project copy.js
@@ -0,0 +1,41 @@
+// API KEY
+const apiKey = 'e1466e603b9570ff2d07c6a19f8de6c8';
+
+document.querySelector('#fetchWeather').addEventListener('click', () => {
+    const location = document.querySelector('#locationInput').value;
+    getWeatherData(location);
+});
+
+function getWeatherData(location) {
+    // Construct the WeatherAPI API URL
+    const apiUrl = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=1&aqi=no&alerts=no`;
+
+    fetch(apiUrl)
+        .then(response => {
+            if (response.status === 200) {
+                return response.json();
+            } else {
+                throw new Error(`Failed to fetch data. Response status: ${response.status}`);
+            }
+        })
+        .then(data => {
+            displayWeatherData(data);
+        })
+        .catch(error => {
+            showError(error.message);
+        });
+}
+
+function displayWeatherData(data) {
+    document.querySelector('#error').textContent = '';
+    document.querySelector('#temperature').textContent = `Temperature: ${data.current.temp_c}°C`;
+    document.querySelector('#description').textContent = `Conditions: ${data.current.condition.text}`;
+    document.querySelector('#weatherIcon').src = data.current.condition.icon;
+}
+
+function showError(message) {
+    document.querySelector('#error').textContent = message;
+    document.querySelector('#temperature').textContent = '';
+    document.querySelector('#description').textContent = '';
+    document.querySelector('#weatherIcon').src = '';
+}
