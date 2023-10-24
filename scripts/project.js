742ea39401f7338e6ff64ca1cb6504eae59d7ef5
Kemish Gomez <113635157+Kemishigp@users.noreply.github.com>
2023-10-24 10:24:25 -0600

    current.js

--- scripts/project.js
+++ scripts/project.js
@@ -9,7 +9,7 @@ document.querySelector('#fetchWeather').addEventListener('click', () => {
 function getWeatherData(location) {
     // Construct the WeatherAPI API URL
                     // http://api.weatherapi.com/v1/forecast.json?key=%20b972ab33e7e440efa30215736232210&q=83440      &days=1&aqi=no&alerts=no
-    // const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}&aqi=no`;
+    const apiUrlC = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}&aqi=no`;
 
     const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=1&aqi=no&alerts=no`;
 
@@ -28,6 +28,29 @@ function getWeatherData(location) {
             showError(error.message);
         });
 }
+
+    fetch(apiUrlC)
+        .then(response => {
+            if (response.status === 200) {
+                return response.json();
+            } else {
+                throw new Error(`Failed to fetch data. Response status: ${response.status}`);
+            }
+        })
+        .then(data => {
+            displayCurrentData(data);
+        })
+        .catch(error => {
+            showError(error.message);
+        });
+}
+function displayCurrentData(data){
+    document.querySelector('#error').textContent = '';
+    document.querySelector('#temperatureC').textContent = `Temperature: ${data.current.temp_c}°C / ${data.current.temp_f}°F`;
+    document.querySelector('#descriptionC').textContent = `Conditions: ${data.current.condition.text}`;
+    document.querySelector('#weatherIconC').src = data.current.condition.icon; 
+}
+
 
 function displayWeatherData(data) {
     document.querySelector('#location').textContent = `Location: ${data.location.name}, ${data.location.country}`;
